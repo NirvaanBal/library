417c352d06a7831209ca392aed8346fae3bd77e9
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-17 11:08:09 +0530

    Create a function to loop over all books in the library and display them on UI

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 
-    this.info = function () {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${
-            this.read ? 'finished' : 'not read yet'
-        }`;
-    };
+    // this.info = function () {
+    //     return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    //         this.read ? 'finished' : 'not read yet'
+    //     }`;
+    // };
 }
 
 const form = document.querySelector('form');
 const error = document.querySelector('.error');
+const books = document.querySelector('.books');
+
 function addBooktoLibrary() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
@@ -23,7 +25,7 @@ function addBooktoLibrary() {
 
     if (!title || !author || !pages || !read) {
         error.style.cssText = 'color: red; font-weight: bold';
-        return;
+        return false;
     }
 
     if (read === 'yes') read = true;
@@ -32,10 +34,32 @@ function addBooktoLibrary() {
     const book = new Book(title, author, +pages, read);
     myLibrary.push(book);
 
+    form.reset();
+
     return book;
 }
 
+const showBooks = function () {
+    books.textContent = 'Adding...';
+    let bookHTML = ``;
+    myLibrary.forEach((book) => {
+        bookHTML += `<div class="book">
+            <h2 class="title">${book.title}</h2>
+            <h3 class="author">${book.author}</h3>
+            <div class="pages">${book.pages}</div>
+            <div class="read">${
+                book.read ? 'Finished reading' : 'Not read yet'
+            }</div>
+        </div>`;
+    });
+
+    books.insertAdjacentHTML('afterbegin', bookHTML);
+};
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    addBooktoLibrary();
+    if (addBooktoLibrary() === false) return;
+
+    error.style.cssText = 'color: #000; font-weight: normal';
+    showBooks();
 });
