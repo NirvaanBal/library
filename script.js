f0898ecd5d47dcd00f620b6ab6cb99c5b9275305
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-17 10:35:24 +0530

    Add HTML form and javascript function to add new book

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = this.read || false;
+    this.read = read;
 
     this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${
@@ -12,3 +12,30 @@ function Book(title, author, pages, read) {
         }`;
     };
 }
+
+const form = document.querySelector('form');
+const error = document.querySelector('.error');
+function addBooktoLibrary() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    let read = document.querySelector('input[name="read"]:checked').value;
+
+    if (!title || !author || !pages || !read) {
+        error.style.cssText = 'color: red; font-weight: bold';
+        return;
+    }
+
+    if (read === 'yes') read = true;
+    else read = false;
+
+    const book = new Book(title, author, +pages, read);
+    myLibrary.push(book);
+
+    return book;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBooktoLibrary();
+});
